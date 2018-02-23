5cb92aadd88416cfc00f07720e05a0ba679a0bf4
Igor Ferreira <fwigorcferreira@users.noreply.github.com>
2018-02-23 12:24:37 +0100

    Change TSL version to fix GitHub connection

--- github_status_helper.rb
+++ github_status_helper.rb
@@ -75,7 +75,7 @@ class GithubStatusHelper
     http = Net::HTTP.new(uri.host, uri.port)
 
     http.use_ssl = true
-    http.ssl_version = :TLSv1
+    http.ssl_version = :SSLv2
     http.verify_mode = OpenSSL::SSL::VERIFY_PEER
 
     req = Net::HTTP::Post.new(uri.path)
